1a39074e12898393b29f4191b7f2efb1d0767019
_haks <1668938504@qq.com>
2021-02-23 22:15:16 +0800

    准备名片操作函数, 修改主文件中函数调用, 框架搭建完成

--- cards_main.py
+++ cards_main.py
@@ -14,13 +14,13 @@ while True:
 
         # 新增名片
         if action_str == '1':
-            pass
+            cards_tools.new_card()
         # 显示全部
         elif action_str == '2':
-            pass
+            cards_tools.show_all()
         # 查询名片
         elif action_str == '3':
-            pass
+            cards_tools.search_card()
 
     # 0 退出系统
     elif action_str == '0':
--- cards_tools.py
+++ cards_tools.py
@@ -1,4 +1,5 @@
 def show_menu():
+
     """显示菜单"""
     print("\\" * 50)
     print("欢迎使用【名片管理系统】version-1.0")
@@ -8,4 +9,25 @@ def show_menu():
     print("3. 搜索名片")
     print("*" * 25)
     print("0. 退出系统")
-    print("\\" * 50)
+    print("/" * 50)
+
+
+def new_card():
+
+    """新增名片"""
+    print("-" * 50)
+    print("* 新增名片")
+
+
+def show_all():
+
+    """显示所有名片"""
+    print("-" * 50)
+    print("* 显示所有名片")
+
+
+def search_card():
+
+    """搜索名片"""
+    print("-" * 50)
+    print("* 搜索名片")
