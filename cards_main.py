63183a6b67d69f9072d591e5eb4b09df1758e1fd
_haks <1668938504@qq.com>
2021-02-23 21:32:19 +0800

    if嵌套判断用户的具体操作, 预留代码位置

--- cards_main.py
+++ cards_main.py
@@ -1,13 +1,24 @@
 # 无限循环, 由用户决定何时退出循环
 while True:
 
+    # 显示功能菜单
+
     action_str = input("请选择希望执行的操作: ")
     print("您选择的操作是【%s】" % action_str)
 
     # 1, 2, 3 针对名片的操作
     if action_str in ['1', '2', '3']:
 
-        pass
+        # 新增名片
+        if action_str == '1':
+            pass
+        # 显示全部
+        elif action_str == '2':
+            pass
+        # 查询名片
+        elif action_str == '3':
+            pass
+
     # 0 退出系统
     elif action_str == '0':
 
