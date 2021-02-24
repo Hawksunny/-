12f44474cd02d6c6f51b8b1f63b4ca763d1fa0b4
_haks <1668938504@qq.com>
2021-02-24 14:09:29 +0800

    新增名片功能实现

--- cards_tools.py
+++ cards_tools.py
@@ -22,6 +22,26 @@ def new_card():
     print("-" * 50)
     print("* 新增名片")
 
+    # 1. 提示用户输入名片的详细信息
+    name_str = input("请输入姓名: ")
+    phone_str = input("请输入电话: ")
+    qq_str = input("请输入QQ: ")
+    email_str = input("请输入邮箱: ")
+
+    # 2. 使用用户的信息建立一个名片字典
+    card_dict = {"name": name_str,
+                 "phone": phone_str,
+                 "qq": qq_str,
+                 "email": email_str}
+
+    # 3. 将名片字典添加到列表中
+    card_list.append(card_dict)
+
+    print(card_list)
+
+    # 4. 提示用户名片添加成功
+    print("添加 %s 的名片成功!" % name_str)
+
 
 def show_all():
 
