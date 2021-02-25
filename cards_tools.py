55eaef18b6eb0182cce5d3cd1f52327d679f624b
_haks <1668938504@qq.com>
2021-02-25 20:23:31 +0800

    添加修改名片、删除名片功能, 查询名片功能完整实现!

--- cards_tools.py
+++ cards_tools.py
@@ -35,10 +35,8 @@ def new_card():
     # 3. 将名片字典添加到列表中
     card_list.append(card_dict)
 
-    print(card_list)
-
     # 4. 提示用户名片添加成功
-    print("添加 %s 的名片成功!" % name_str)
+    print("成功添加 %s 的名片!" % name_str)
 
 
 def show_all():
@@ -48,7 +46,7 @@ def show_all():
 
     # 判断是否存在名片记录, 如果没有, 提示用户并且返回
     if len(card_list) == 0:
-        print("当前没有任何名片记录, 请使用功能1新增名片吧!")
+        print("当前没有任何名片记录, 请使用功能【1】新增名片吧!")
 
         # return 可以返回一个函数的执行结果
         # 下方的代码不会被执行
@@ -85,7 +83,6 @@ def search_card():
     for card_dict in card_list:
 
         if card_dict["name"] == find_name:
-
             print("姓名\t\t\t电话\t\t\tQQ\t\t\t邮箱")
 
             print("=" * 50)
@@ -95,10 +92,63 @@ def search_card():
                                                   card_dict["qq"],
                                                   card_dict["email"]))
 
-            # TODO 针对找到的名片记录执行修改和删除的操作
+            print("=" * 50)
+
+            # 针对找到的名片记录执行修改和删除的操作
+            deal_card(card_dict)
 
             break
 
     else:
 
         print("抱歉, 没有找到 %s" % find_name)
+
+
+def deal_card(find_dict):
+    """编辑查找到的名片
+
+    :param find_dict: 查找到的名片
+    """
+    print("[1] 修改\t[2] 删除\t[0] 返回上级菜单")
+
+    action_str = input("请选择要执行的操作：")
+
+    if action_str == '1':
+
+        find_dict["name"] = input_card_info(find_dict["name"],
+                                            "新的姓名[回车不修改]：")
+        find_dict["phone"] = input_card_info(find_dict["phone"],
+                                             "新的电话[回车不修改]：")
+        find_dict["qq"] = input_card_info(find_dict["qq"],
+                                          "新的QQ[回车不修改]：")
+        find_dict["email"] = input_card_info(find_dict["email"],
+                                             "新的邮箱[回车不修改]：")
+
+        print("%s 已被修改!" % find_dict["name"])
+
+    elif action_str == '2':
+
+        card_list.remove(find_dict)
+
+        print("%s 已被删除！" % find_dict["name"])
+
+
+def input_card_info(dict_value, tip_message):
+    """输入名片信息
+
+    :param dict_value: 字典中原有的值
+    :param tip_message: 提示用户输入的提示文字
+    :return: 如果用户输入了内容, 就返回内容, 否则就返回字典中原有的值
+    """
+    # 1. 提示用户输入内容
+    result_str = input(tip_message)
+
+    # 2. 针对用户的输入进行判断, 如果用户输入了内容, 直接返回结果
+    if len(result_str) > 0:
+
+        return result_str
+
+    # 3. 如果用户没有输入内容, 返回'字典中原有的值'
+    else:
+
+        return dict_value
